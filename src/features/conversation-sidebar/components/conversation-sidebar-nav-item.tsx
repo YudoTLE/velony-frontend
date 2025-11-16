18ce1bc08a4b1954573188624d02f9c50be55c04
YudoTLE <yudojago@gmail.com>
2025-11-16 22:48:52 +0700

    feat: refactor ConversationSidebarNavItem to use Avatar component and improve layout

--- src/features/conversation-sidebar/components/conversation-sidebar-nav-item.tsx
+++ src/features/conversation-sidebar/components/conversation-sidebar-nav-item.tsx
@@ -1,4 +1,9 @@
-import type { Conversation } from '@shared/schemas/conversation.schema';
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '@shared/components/ui/avatar';
+import type { Conversation } from '@shared/types/conversation';
 import { formatDistanceToNow } from 'date-fns';
 import Link from 'next/link';
 
@@ -12,21 +17,28 @@ export const ConversationSidebarNavItem = ({
   return (
     <Link
       href={data.url}
-      className="hover:bg-sidebar-accent hover:text-sidebar-accent-foreground flex flex-col items-start gap-2 border-b px-4 py-3 text-sm leading-tight whitespace-nowrap first:border-t"
+      className="hover:bg-sidebar-accent hover:text-sidebar-accent-foreground flex items-center gap-2 border-b px-3 py-2 text-sm leading-tight first:border-t text-muted-foreground"
     >
-      <div className="flex w-full items-center gap-2">
-        <span>{data.title}</span>
-        <span className="ml-auto text-xs">
-          {data.lastMessage &&
-            formatDistanceToNow(new Date(data.lastMessage.updatedAt), {
-              addSuffix: true,
-            })}
+      <Avatar className="size-10 shrink-0">
+        <AvatarImage src={data.thumbnailUrl || undefined} alt={data.title} />
+        <AvatarFallback>{data.title.slice(0, 2).toUpperCase()}</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col gap-2 flex-1 min-w-0">
+        <div className="flex w-full items-center gap-2">
+          <span className="text-foreground truncate">{data.title}</span>
+          <span className="ml-auto text-[8px] shrink-0">
+            {data.lastMessage?.updatedAt &&
+              formatDistanceToNow(new Date(data.lastMessage?.updatedAt), {
+                addSuffix: true,
+              })}
+          </span>
+        </div>
+        <span className="line-clamp-1 text-xs">
+          {data.lastMessage
+            ? `${data.lastMessage.senderName}: ${data.lastMessage.content}`
+            : '-- no message --'}
         </span>
       </div>
-      <span className="line-clamp-1 w-[260px] text-xs whitespace-break-spaces">
-        {data.lastMessage &&
-          `${data.lastMessage.senderName}: ${data.lastMessage.content}`}
-      </span>
     </Link>
   );
 };
